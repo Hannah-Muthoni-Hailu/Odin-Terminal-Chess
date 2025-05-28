d9d3b7e5b3cfa9e83c353a9f9e901633b2f0e1d3
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-05-28 14:41:25 +0300

    Pawn functionality completed

---
+++ lib/pawn.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Controls the pawn on the board
+class Pawn
+  def initialize(location)
+    @location = location
+    @no_of_moves = 0
+  end
+
+  def step_forward(new_loc)
+    # Check that the move is in the same column
+    return false unless new_loc[0] == @location[0]
+
+    # Check that the move is only one step forward
+    # or is 2 steps forward if number of moves is zero
+    if (@no_of_moves.zero? && new_loc[1] == @location[1] + 2) || new_loc[1] == @location[1] + 1
+      @location = new_loc
+      @no_of_moves += 1
+      true
+    else
+      false
+    end
+  end
+
+  def eat(target)
+    if (target[1] == @location[1] + 1) && (target[0] == @location[0] - 1 || target[0] == @location[0] + 1)
+      @location = target
+      @no_of_moves += 1
+      return true
+    end
+    false
+  end
+end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require_relative '../lib/pawn'
+
+describe Pawn do
+  let(:pawn) { described_class.new([1, 0]) }
+  describe '#step_forward' do
+    context 'when a valid location is given' do
+      it 'allows the player to move one step forward' do
+        expect(pawn.step_forward([1, 1])).to be true
+      end
+
+      it 'changes the pawns location' do
+        pawn.step_forward([1, 1])
+        expect(pawn.instance_variable_get(:@location)).to eq([1, 1])
+      end
+
+      it 'updates no_of_moves' do
+        pawn.step_forward([1, 1])
+        expect(pawn.instance_variable_get(:@no_of_moves)).to eq(1)
+      end
+    end
+
+    context 'when an invalid move is made' do
+      before do
+        pawn.instance_variable_set(:@no_of_moves, 1)
+      end
+      it 'returns false' do
+        expect(pawn.step_forward([1, 2])).to be false
+      end
+
+      it 'does not update the pawns location' do
+        pawn.step_forward([1, 2])
+        expect(pawn.instance_variable_get(:@location)).to eq([1, 0])
+      end
+    end
+
+    context 'when it is the pawns first move' do
+      before do
+        pawn.instance_variable_set(:@no_of_moves, 0)
+      end
+      it 'allows the player to move two steps forwards' do
+        expect(pawn.step_forward([1, 2])).to be true
+      end
+    end
+  end
+
+  describe '#eat' do
+    it 'allows the pawn to eat right' do
+      expect(pawn.eat([0, 1])).to be true
+    end
+
+    it 'updates the pawns location' do
+      new_loc = [0, 1]
+      pawn.eat(new_loc)
+      expect(pawn.instance_variable_get(:@location)).to eq(new_loc)
+    end
+
+    it 'allows the pawn to eat right' do
+      expect(pawn.eat([2, 1])).to be true
+    end
+  end
+end

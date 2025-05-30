c2ca4768d5b182e96129242ce889cb1b8d6468f8
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-05-30 12:40:47 +0300

    Move function updated for queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -10,12 +10,66 @@ class Queen
   end
 
   def move(new_location)
-    if ((new_location[0] == @location[0]) ^ (new_location[1] == @location[1])) ||
-       ((@location[0] - new_location[0]).abs == (@location[1] - new_location[1]).abs)
+    if (new_location[0] == @location[0]) ^ (new_location[1] == @location[1])
+      return move_diag(new_location)
+    elsif (@location[0] - new_location[0]).abs == (@location[1] - new_location[1]).abs
+      return move_straight(new_location)
+    end
+
+    false
+  end
+
+  def move_diag(new_location)
+    unless block_given?
       @location = new_location
       return true
     end
-    false
+    counter = -1
+    if @location[0] < new_location[0] && @location[1] < new_location[1]
+      until @location == new_location
+        @location[0] += 1
+        @location[1] += 1
+        counter += 1
+        yield @location, counter
+      end
+    elsif @location[0] < new_location[0] && @location[1] > new_location[1]
+      until @location == new_location
+        @location[0] += 1
+        @location[1] -= 1
+        counter += 1
+        yield @location, counter
+      end
+    elsif @location[0] > new_location[0] && @location[1] > new_location[1]
+      until @location == new_location
+        @location[0] -= 1
+        @location[1] -= 1
+        counter += 1
+        yield @location, counter
+      end
+    elsif @location[0] > new_location[0] && @location[1] < new_location[1]
+      until @location == new_location
+        @location[0] -= 1
+        @location[1] += 1
+        counter += 1
+        yield @location, counter
+      end
+    end
+    true
+  end
+
+  def move_straight(new_loc)
+    if new_loc[0] == @location[0]
+      until @location == new_loc
+        new_loc[1] > @location[1] ? @location[1] += 1 : @location[1] -= 1
+        yield @location if block_given?
+      end
+    elsif new_loc[1] == @location[1]
+      until @location == new_loc
+        new_loc[0] > @location[0] ? @location[0] += 1 : @location[0] -= 1
+        yield @location if block_given?
+      end
+    end
+    true
   end
 
   def to_s

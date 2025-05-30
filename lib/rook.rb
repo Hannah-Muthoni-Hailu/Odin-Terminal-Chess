780b18c9fe5c225901831a3c6317cc287345c96f
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-05-30 12:38:24 +0300

    Move function updated for rook

--- lib/rook.rb
+++ lib/rook.rb
@@ -9,9 +9,18 @@ class Rook
     @type = type
   end
 
-  def move(new_loc)
-    if (new_loc[0] == @location[0]) ^ (new_loc[1] == @location[1])
-      @location = new_loc
+  def move(new_loc) # rubocop:disable Metrics/MethodLength
+    if new_loc[0] == @location[0]
+      until @location == new_loc
+        new_loc[1] > @location[1] ? @location[1] += 1 : @location[1] -= 1
+        yield @location if block_given?
+      end
+      return true
+    elsif new_loc[1] == @location[1]
+      until @location == new_loc
+        new_loc[0] > @location[0] ? @location[0] += 1 : @location[0] -= 1
+        yield @location if block_given?
+      end
       return true
     end
     false

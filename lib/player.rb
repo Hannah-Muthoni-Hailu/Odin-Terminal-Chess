a95f0d6a4a784e91347874411723038066a99ca8
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-06-01 13:03:16 +0300

    Player completed

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,28 @@ class Player
   def initialize(type)
     @type = type
   end
+
+  def make_move # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
+    p 'Select the piece you want to move'
+    p 'The first value represents the vertical values and the second represents horizontal values'
+    move = gets.chomp.split(',').map!(&:strip).map do |val|
+      return 'Check Mate' if val.downcase == 'c'
+      return 'Invalid input' unless val.to_i.to_s == val && val.to_i < 7
+
+      val.to_i
+    end
+
+    p 'Select the location you want to move the piece to'
+    target = gets.chomp.split(',').map!(&:strip).map do |val|
+      return 'Check Mate' if val.downcase == 'c'
+      return 'Invalid input' unless val.to_i.to_s == val && val.to_i < 7
+
+      val.to_i
+    end
+
+    [move, target]
+  end
 end
+
+player = Player.new(:black)
+p player.make_move
